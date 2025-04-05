bba0c15cc0a377ad7385a843a80de81dae60f787
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-05 18:25:25 +0900

    feat(問題1) : 問題1を実装した。

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,5 +1,7 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
+CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
