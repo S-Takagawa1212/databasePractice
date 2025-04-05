c659b7f3f197f68c6de1a94f1d862f5129c57d25
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-05 20:07:29 +0900

    feat(問題2): エラーを修正

--- pg_practice.sql
+++ pg_practice.sql
@@ -4,12 +4,11 @@ CREATE DATABASE practice;
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 CREATE TYPE gender AS ENUM('Man','Woman','Other');
 
-CREATE TABLE user (
-  id int SERIAL NOT NULL,
-  name CHAR(255) NOT NULL DEFAULT '',
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL DEFAULT '',
   age int NOT NULL DEFAULT 0,
-  gender gender DEFAULT 'Other',
-  PRIMARY KEY (id),
+  gender gender DEFAULT 'Other'
 );
 
 COMMENT ON COLUMN user.name IS '氏名';
