0c2f17093494c2bd6282a0151c549240bf8cb3b6
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-05 19:59:35 +0900

    feat(問題2): 問題2を実装

--- pg_practice.sql
+++ pg_practice.sql
@@ -2,6 +2,18 @@
 CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+CREATE TYPE gender AS ENUM('Man','Woman','Other');
 
+CREATE TABLE user (
+  id int SERIAL NOT NULL,
+  name CHAR(255) NOT NULL DEFAULT '',
+  age int NOT NULL DEFAULT 0,
+  gender gender DEFAULT 'Other',
+  PRIMARY KEY (id),
+);
+
+COMMENT ON COLUMN user.name IS '氏名';
+COMMENT ON COLUMN user.age IS '年齢';
+COMMENT ON COLUMN user.gender IS '性別';
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
