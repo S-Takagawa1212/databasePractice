6c54af0a98c9697431d8461a34b2cb344401be6c
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-08 00:32:40 +0900

    feat(practice): 問題22を実装

--- practice.sql
+++ practice.sql
@@ -1,169 +1,168 @@
--- 問1
--- 国名を全て抽出してください。
+  -- 問1
+  -- 国名を全て抽出してください。
 
-SELECT name
-FROM countries;
+  SELECT name
+  FROM countries;
 
--- 問2
--- ヨーロッパに属する国をすべて抽出してください。
+  -- 問2
+  -- ヨーロッパに属する国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area
-FROM countries
-WHERE continent = 'Europe';
+  SELECT code, name, continent, region, surface_area
+  FROM countries
+  WHERE continent = 'Europe';
 
--- 問3
--- ヨーロッパ以外に属する国をすべて抽出してください。
+  -- 問3
+  -- ヨーロッパ以外に属する国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area
-FROM countries
-WHERE continent != 'Europe';
+  SELECT code, name, continent, region, surface_area
+  FROM countries
+  WHERE continent != 'Europe';
 
--- 問4
--- 人口が10万人以上の国をすべて抽出してください。
+  -- 問4
+  -- 人口が10万人以上の国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area, indep_year, population
-FROM countries
-WHERE population >= 100000;
+  SELECT code, name, continent, region, surface_area, indep_year, population
+  FROM countries
+  WHERE population >= 100000;
 
--- 問5
--- 平均寿命が56歳から76歳の国をすべて抽出してください。
+  -- 問5
+  -- 平均寿命が56歳から76歳の国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area
-FROM countries
-WHERE life_expectancy BETWEEN 56 AND 76;
+  SELECT code, name, continent, region, surface_area
+  FROM countries
+  WHERE life_expectancy BETWEEN 56 AND 76;
 
--- 問6
--- 国コードがNLB,ALB,DZAのもの市区町村をすべて抽出してください。
+  -- 問6
+  -- 国コードがNLB,ALB,DZAのもの市区町村をすべて抽出してください。
 
-SELECT cities.id, cities.name, countries.code, cities.district, countries.population
-FROM cities
-  LEFT OUTER JOIN countries
-    ON cities._code = countries.code
-WHERE cities.country_code = 'NLB'
-  OR  cities.country_code = 'ALB'
-  OR  cities.country_code = 'DZA';
+  SELECT cities.id, cities.name, countries.code, cities.district, countries.population
+  FROM cities
+    LEFT OUTER JOIN countries
+      ON cities._code = countries.code
+  WHERE cities.country_code = 'NLB'
+    OR  cities.country_code = 'ALB'
+    OR  cities.country_code = 'DZA';
 
--- 問7
--- 独立独立記念日がない国をすべて抽出してください。
+  -- 問7
+  -- 独立独立記念日がない国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area, indep_year
-FROM countries
-WHERE indep_year IS NULL;
+  SELECT code, name, continent, region, surface_area, indep_year
+  FROM countries
+  WHERE indep_year IS NULL;
 
--- 問8
--- 独立独立記念日がある国をすべて抽出してください。
+  -- 問8
+  -- 独立独立記念日がある国をすべて抽出してください。
 
-SELECT code, name, continent, region, surface_area, indep_year
-FROM countries
-WHERE indep_year IS NOT NULL;
+  SELECT code, name, continent, region, surface_area, indep_year
+  FROM countries
+  WHERE indep_year IS NOT NULL;
 
--- 問9
--- 名前の末尾が「ia」で終わる国を抽出してください。
+  -- 問9
+  -- 名前の末尾が「ia」で終わる国を抽出してください。
 
-SELECT code, name, continent, region, surface_area
-FROM countries
-WHERE name LIKE '%ia';
+  SELECT code, name, continent, region, surface_area
+  FROM countries
+  WHERE name LIKE '%ia';
 
--- 問10
--- 名前の中に「st」が含まれる国を抽出してください。
--- ILIKEはpostgreSQL限定で大文字小文字区別なしで比較可能
+  -- 問10
+  -- 名前の中に「st」が含まれる国を抽出してください。
+  -- ILIKEはpostgreSQL限定で大文字小文字区別なしで比較可能
 
-SELECT code, name, continent, region, surface_area
-FROM countries
-WHERE name ILIKE '%st%';
+  SELECT code, name, continent, region, surface_area
+  FROM countries
+  WHERE name ILIKE '%st%';
 
--- 問11
--- 名前が「an」で始まる国を抽出してください。
+  -- 問11
+  -- 名前が「an」で始まる国を抽出してください。
 
-SELECT code, name, continent, region, surface_area, indep_year
-FROM countries
-WHERE name ILIKE 'an%';
+  SELECT code, name, continent, region, surface_area, indep_year
+  FROM countries
+  WHERE name ILIKE 'an%';
 
--- 問12
--- 全国の中から独立記念日が1990年より前または人口が10万人より多い国を全て抽出してください。
-SELECT code, name, continent, region, surface_area, indep_year,population
-FROM countries
-WHERE indep_year < 1990
-  OR population > 100000;
+  -- 問12
+  -- 全国の中から独立記念日が1990年より前または人口が10万人より多い国を全て抽出してください。
+  SELECT code, name, continent, region, surface_area, indep_year,population
+  FROM countries
+  WHERE indep_year < 1990
+    OR population > 100000;
 
--- 問13
--- コードがDZAもしくはALBかつ独立記念日が1990年より前の国を全て抽出してください。
+  -- 問13
+  -- コードがDZAもしくはALBかつ独立記念日が1990年より前の国を全て抽出してください。
 
-SELECT code, name, continent, region, surface_area, indep_year,population
-FROM countries
-WHERE code IN ('DZA', 'ALB')
-  AND indep_year < 1990;
+  SELECT code, name, continent, region, surface_area, indep_year,population
+  FROM countries
+  WHERE code IN ('DZA', 'ALB')
+    AND indep_year < 1990;
 
--- 問14
--- 全ての地方をグループ化せずに表示してください。
+  -- 問14
+  -- 全ての地方をグループ化せずに表示してください。
 
-SELECT DISTINCT region
-FROM countries
-ORDER BY region;
-
-
--- 問15
--- 国名と人口を以下のように表示させてください。シングルクォートに注意してください。
--- 「Arubaの人口は103000人です」
-
-SELECT name || 'の人口は' || population || '人です。'
-FROM countries ;
+  SELECT DISTINCT region
+  FROM countries
+  ORDER BY region;
 
 
--- 問16
--- 平均寿命が短い順に国名を表示させてください。ただしNULLは表示させないでください。
-SELECT name, life_expectancy
-FROM countries
-WHERE life_expectancy IS  NOT NULL
-ORDER BY life_expectancy ;
+  -- 問15
+  -- 国名と人口を以下のように表示させてください。シングルクォートに注意してください。
+  -- 「Arubaの人口は103000人です」
+
+  SELECT name || 'の人口は' || population || '人です。'
+  FROM countries ;
 
 
--- 問17
--- 平均寿命が長い順に国名を表示させてください。ただしNULLは表示させないでください。
-SELECT name, life_expectancy
-FROM countries
-WHERE life_expectancy IS  NOT NULL
-ORDER BY life_expectancy desc;
-
--- 問18
--- 平均寿命が長い順、独立記念日が新しい順に国を表示させてください。
-
-SELECT name, life_expectancy, indep_year
-FROM countries
-WHERE life_expectancy IS NOT NULL
-  OR indep_year IS NOT NULL
-ORDER BY life_expectancy desc, indep_year desc;
-
--- 問19
--- 全ての国の国コードの一文字目と国名を表示させてください。
-
-SELECT left(code, 1) AS first_char, name
-FROM countries ;
+  -- 問16
+  -- 平均寿命が短い順に国名を表示させてください。ただしNULLは表示させないでください。
+  SELECT name, life_expectancy
+  FROM countries
+  WHERE life_expectancy IS  NOT NULL
+  ORDER BY life_expectancy ;
 
 
--- 問20
--- 国名が長いものから順に国名と国名の長さを出力してください。
+  -- 問17
+  -- 平均寿命が長い順に国名を表示させてください。ただしNULLは表示させないでください。
+  SELECT name, life_expectancy
+  FROM countries
+  WHERE life_expectancy IS  NOT NULL
+  ORDER BY life_expectancy desc;
 
-SELECT name, LENGTH(name) AS name_length
-FROM countries
-ORDER BY name_length desc;
+  -- 問18
+  -- 平均寿命が長い順、独立記念日が新しい順に国を表示させてください。
 
--- 問21
--- 全ての地方の平均寿命、平均人口を表示してください。(NULLも表示)
+  SELECT name, life_expectancy, indep_year
+  FROM countries
+  WHERE life_expectancy IS NOT NULL
+    OR indep_year IS NOT NULL
+  ORDER BY life_expectancy desc, indep_year desc;
 
-SELECT region, AVG(life_expectancy) AS "平均寿命", AVG(population) AS "平均人口"
-FROM countries
-GROUP BY region
-ORDER BY region ;
+  -- 問19
+  -- 全ての国の国コードの一文字目と国名を表示させてください。
 
--- 問22
--- 全ての地方の最長寿命、最大人口を表示してください。(NULLも表示)
+  SELECT left(code, 1) AS first_char, name
+  FROM countries ;
 
-SELECT
-FROM
-WHERE
-  OR
-ORDER BY
+
+  -- 問20
+  -- 国名が長いものから順に国名と国名の長さを出力してください。
+
+  SELECT name, LENGTH(name) AS name_length
+  FROM countries
+  ORDER BY name_length desc;
+
+  -- 問21
+  -- 全ての地方の平均寿命、平均人口を表示してください。(NULLも表示)
+
+  SELECT region, AVG(life_expectancy) AS "平均寿命", AVG(population) AS "平均人口"
+  FROM countries
+  GROUP BY region
+  ORDER BY region ;
+
+  -- 問22
+  -- 全ての地方の最長寿命、最大人口を表示してください。(NULLも表示)
+
+  SELECT region, MAX(life_expectancy) AS "最長寿命", MAX(population) AS "最大人口"
+  FROM countries
+  GROUP BY region
+  ORDER BY region ;
 
 -- 問23
 -- アジア大陸の中で最小の表面積を表示してください
